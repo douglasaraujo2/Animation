155ce240ab1d69bb20d74aa79b20121eec2ff0ae
logonrm <logonrm@fiap.com.br>
2018-02-05 21:39:33 -0200

    Project commit

---
+++ Assets/Scripts/AnimaScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimaScript : MonoBehaviour {
+
+	public float velocidade;
+
+	Animator animator;
+	SpriteRenderer sprite;
+	// Use this for initialization
+	void Start () {
+		//Interface par ao componente animator
+		animator = GetComponent<Animator> ();
+		sprite = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Mover o personagem
+		float mover_x = Input.GetAxisRaw ("Horizontal") *velocidade * Time.deltaTime;
+		transform.Translate (mover_x, 0.0f, 0.0f);
+
+		//Reproduzir animação
+		if (mover_x < 0.0f) {
+			sprite.flipX = true;
+		} else if(mover_x > 0.0f) {
+			sprite.flipX = false;
+		}
+		animator.SetFloat("pMover",Mathf.Abs(Input.GetAxisRaw("Horizontal")));
+	}
+}
